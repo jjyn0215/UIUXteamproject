119d6d0ad57cf2bbeb4328bb736667ccce72e148
Ryzenserver <jjyn0215@gmail.com>
2026-05-24 16:57:58 +0900

    Fix launch route unit test dependency

--- synced_alarm/android/app/src/test/kotlin/com/teamproject/synced_alarm/LaunchRouteDecisionTest.kt
+++ synced_alarm/android/app/src/test/kotlin/com/teamproject/synced_alarm/LaunchRouteDecisionTest.kt
@@ -1,8 +1,8 @@
 package com.teamproject.synced_alarm
 
-import kotlin.test.Test
-import kotlin.test.assertFalse
-import kotlin.test.assertTrue
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Test
 
 class LaunchRouteDecisionTest {
     @Test
